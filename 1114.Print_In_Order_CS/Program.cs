2315b9abc1a913b0482951f0dbbac81ede016cf9
Connor McDermott <connormcderm@protonmail.com>
2025-07-28 12:27:48 -0500

    1114 and 1117 from desktop

---
+++ 1114.Print_In_Order_CS/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Solution {
+    public class Foo {
+        private Semaphore FirstSemaphore = new Semaphore(0, 1);
+        private Semaphore SecondSemaphore = new Semaphore(0, 1);
+
+        public Foo() {}
+
+        public void First(Action printFirst) {
+
+            // printFirst() outputs "first". Do not change or remove this line.
+            printFirst();
+
+            FirstSemaphore.Release();
+        }
+
+        public void Second(Action printSecond) {
+
+            this.FirstSemaphore.WaitOne();
+            // printSecond() outputs "second". Do not change or remove this line.
+            printSecond();
+
+            this.SecondSemaphore.Release();
+        }
+
+        public void Third(Action printThird) {
+            this.SecondSemaphore.WaitOne();
+            // printThird() outputs "third". Do not change or remove this line.
+            printThird();
+        }
+    }
+
+}
---
+++ 1117.Building_H2O_CS/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Solution {
+    public class H2O {
+
+        private int count = 0;
+        private object hLock = new object();
+        private SemaphoreSlim HydrogenSemaphore = new SemaphoreSlim(2);
+        private SemaphoreSlim OxygenSemaphore = new SemaphoreSlim(1);
+
+        public H2O() {}
+
+        public void Hydrogen(Action releaseHydrogen) {
+            HydrogenSemaphore.Wait();
+            releaseHydrogen();
+
+            // releaseHydrogen() outputs "H". Do not change or remove this line.
+            lock(hLock) {
+                count++;
+                if (count == 2) {
+                    count = 0;
+                    OxygenSemaphore.Release();
+                }
+            }
+        }
+
+        public void Oxygen(Action releaseOxygen) {
+            OxygenSemaphore.Wait();
+
+            // releaseOxygen() outputs "O". Do not change or remove this line.
+            releaseOxygen();
+            HydrogenSemaphore.Release(2);
+        }
+    }
+}
