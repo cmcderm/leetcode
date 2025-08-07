cc4b440744e11b4c5efb321fe00a6d4e5c9715cc
Connor McDermott <connormcderm@protonmail.com>
2025-08-07 16:15:42 -0500

    Hit a deadend with the cool soln

--- 240.Search_2dMatrix_C/main.c
+++ 240.Search_2dMatrix_C/main.c
@@ -2,8 +2,8 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-const int MATRIX_SIZE = 5;
-const int MATRIX_COLSIZE = 5;
+static const int MATRIX_SIZE = 5;
+static const int MATRIX_COLSIZE = 5;
 
 void matrix_print(int** matrix, int matrixSize, int matrixColSize) {
     for (int i = 0; i < matrixSize; i++) {
@@ -14,46 +14,42 @@ void matrix_print(int** matrix, int matrixSize, int matrixColSize) {
     }
 }
 
-bool searchMatrix(int** matrix, int matrixSize, int* matrixColSize, int target){
+bool searchMatrix(int** matrix, const int matrixSize, const int* matrixColSize, const int target){
     int left = 0;
-    int right = matrixSize;
+    int right = matrixSize - 1;
 
-    int column = -1;
+    // I'm imagining a 2d binary sort, first searching vertically, then horizontally
+    // Outer then inner, I don't think binary search benefits from locality caching, but it feels logical
+    // Best I've got is O(log(n)log(m))
+    while (left <= right) {
+        int mid_row = (left + right) / 2;
 
-    while (left < right) {
-        int mid_column = (left + right) / 2;
 
-        int mid_top = matrix[0][mid_column];
-        int mid_bottom = matrix[matrixSize-1][mid_column];
+        int mid_top = matrix[mid_row][0];
+        int mid_bottom = matrix[mid_row][(*matrixColSize) - 1];
 
         if (target < mid_top) {
-            right = mid_column - 1;
+            right = mid_row - 1;
         } else if (target > mid_bottom){
-            left = mid_column + 1;
+            left = mid_row + 1;
         } else {
-            column = mid_column;
-            break;
-        }
-    }
+            int top = 0;
+            int bottom = (*matrixColSize) - 1;
 
-    if (column == -1) {
-        return false;
-    }
+            // This inner search doesn't give us any information regarding which side to split
+            while (top <= bottom) {
+                int mid = (top + bottom) / 2;
 
-    left = 0;
-    right = (*matrixColSize);
+                int mid_val = matrix[mid_row][mid];
 
-    while (left < right) {
-        int mid = (left + right) / 2;
-
-        int mid_value = matrix[mid][column];
-
-        if (target < mid_value) {
-            right = mid - 1;
-        } else if (target > mid_value){
-            left = mid + 1;
-        } else {
-            return true;
+                if (target < mid_val ) {
+                    bottom = mid - 1;
+                } else if (target > mid_val) {
+                    top = mid + 1;
+                } else {
+                    return true;
+                }
+            }
         }
     }
 
@@ -69,9 +65,15 @@ int main() {
         {18, 21, 23, 26, 30}
     };
 
-    int** matrix = malloc(MATRIX_SIZE * MATRIX_COLSIZE * sizeof(int*));
+    int** matrix = malloc(MATRIX_SIZE * sizeof(int*));
+    if (matrix == NULL) {
+        perror("Outer malloc failed");
+        exit(1);
+    }
 
     for (int i = 0; i < MATRIX_SIZE; i++) {
+        matrix[i] = malloc(MATRIX_COLSIZE * sizeof(int));
+
         for (int j = 0; j < MATRIX_COLSIZE; j++) {
             matrix[i][j] = ref_matrix[i][j];
         }
@@ -87,7 +89,11 @@ int main() {
         printf("Not found??\n");
     }
 
-    free(*matrix);
+    for (int i = 0; i < MATRIX_SIZE; ++i) {
+        free(matrix[i]);
+    }
+
+    free(matrix);
 
     return 0;
 }
